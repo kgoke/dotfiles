3ba240442c5a5a4353f6043178a05ca953cb6e82
kgoke <kg333920@ohio.edu>
2025-01-05 15:25:44 -0500

    basic nvim

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+-- set line numbers
+vim.opt.number = true
+
+-- ignore case when searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- remove search highlights from previous searches
+vim.opt.hlsearch = false
+
+-- wrap long lines and perserve indent when wraped
+vim.opt.wrap = true
+vim.opt.breakindent = true
+
+-- modify spacing for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+-- highlight current line
+vim.wo.cursorline = true
